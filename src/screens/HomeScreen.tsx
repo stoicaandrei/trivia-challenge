2abca94b4c1e11c40ba339b6cfeb6e7d6ef1ca94
stoicaandrei <andrei@stoica.dev>
2022-08-19 11:53:24 +0300

    Add onBegin to homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,16 @@
 import { QuizLayout } from '~/layouts';
 
-export const HomeScreen = () => {
+type Props = {
+  onBegin: () => void;
+};
+
+export const HomeScreen = ({ onBegin }: Props) => {
   return (
     <QuizLayout>
       <h1 className='font-semibold'>Welcome to the Trivia Challenge!</h1>
       <p>You will be presented with 10 True or False questions.</p>
       <p>Can you score 100%</p>
-      <button>BEGIN</button>
+      <button onClick={onBegin}>BEGIN</button>
     </QuizLayout>
   );
 };
