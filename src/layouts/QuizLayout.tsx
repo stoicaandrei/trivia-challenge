87915e85ae6cd41c88f7e0404512431e8c8db6b4
stoicaandrei <andrei@stoica.dev>
2022-08-19 11:41:19 +0300

    Change padding of quizLayout

--- src/layouts/QuizLayout.tsx
+++ src/layouts/QuizLayout.tsx
@@ -7,7 +7,7 @@ type Props = {
 export const QuizLayout = ({ headline, body, footer }: Props) => {
     return (
         <div className="w-screen h-screen flex items-center justify-center">
-            <div className="w-full h-full max-h-[700px] max-w-[400px] bg-slate-200 flex flex-col justify-between items-center p-3">
+            <div className="w-full h-full max-h-[700px] max-w-[400px] bg-slate-200 flex flex-col justify-between items-center py-3 px-10">
                 {headline}
                 {body}
                 {footer}
