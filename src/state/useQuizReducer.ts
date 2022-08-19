5e6cba0fe00529be7b97d96fd9d28805d943a56a
stoicaandrei <andrei@stoica.dev>
2022-08-19 12:33:09 +0300

    Change state structure

--- src/state/useQuizReducer.ts
+++ src/state/useQuizReducer.ts
@@ -4,7 +4,7 @@ import { Question } from './types';
 type QuizState = {
   questions: Question[];
   currentQuestion: number;
-  score: number;
+  answers: boolean[];
   isFinished: boolean;
   isStarted: boolean;
   isReady: boolean;
@@ -13,7 +13,7 @@ type QuizState = {
 const intialState: QuizState = {
   questions: [],
   currentQuestion: 0,
-  score: 0,
+  answers: [],
   isFinished: false,
   isStarted: false,
   isReady: false,
@@ -40,7 +40,7 @@ type QuizAction =
   | AnswerQuizAction
   | SetQuestionsAction;
 
-const quizReducer = (state: QuizState, action: QuizAction) => {
+const quizReducer = (state: QuizState, action: QuizAction): QuizState => {
   switch (action.type) {
     case 'START_QUIZ':
       return {
@@ -48,7 +48,7 @@ const quizReducer = (state: QuizState, action: QuizAction) => {
         isStarted: true,
         isFinished: false,
         currentQuestion: 0,
-        score: 0,
+        answers: [],
       };
     case 'RESET_QUIZ':
       return {
@@ -56,16 +56,16 @@ const quizReducer = (state: QuizState, action: QuizAction) => {
         isStarted: false,
         isFinished: false,
         currentQuestion: 0,
-        score: 0,
+        answers: [],
       };
     case 'ANSWER_QUESTION':
       const { answer } = action.payload;
-      const { currentQuestion, score } = state;
+      const { currentQuestion, answers } = state;
       const isCorrect = state.questions[currentQuestion].correct_answer === answer;
       return {
         ...state,
         currentQuestion: currentQuestion + 1,
-        score: isCorrect ? score + 1 : score,
+        answers: [...answers, isCorrect],
         isFinished: currentQuestion + 1 === state.questions.length,
       };
     case 'SET_QUESTIONS':
