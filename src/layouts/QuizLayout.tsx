dfca7b22d34b855179f86c19cfdadc40b1d75e45
stoicaandrei <andrei@stoica.dev>
2022-08-19 11:32:13 +0300

    Create QuizLayout component

---
+++ src/layouts/QuizLayout.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    headline?: React.ReactNode;
+    body?: React.ReactNode;
+    footer?: React.ReactNode;
+}
+
+export const QuizLayout = ({ headline, body, footer }: Props) => {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center">
+            <div className="w-full h-full max-h-[700px] max-w-[400px] bg-slate-200 flex flex-col justify-between items-center p-3">
+                {headline}
+                {body}
+                {footer}
+            </div>
+        </div>
+    );
+}
